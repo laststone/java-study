4bc531b4fb7432ed2a95d5363cec06ad0299da06
lastsonic <yang.hu@cloudwiz.cn>
2019-03-31 07:41:51 +0800

    4th

---
+++ day01/src/Dome1_DataType.java
@@ -0,0 +1,14 @@
+public class Dome1_DataType {
+    public static void main(String[] args){
+        byte b = 10;
+        short s = 20;
+        int i = 30;
+        long x = 40;
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(x);
+
+
+    }
+}
