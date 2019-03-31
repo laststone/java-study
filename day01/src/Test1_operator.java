b2481e691355b41c50c448a1c542457f7587a642
lastsonic <yang.hu@cloudwiz.cn>
2019-03-31 09:37:22 +0800

    10th

---
+++ day01/src/Test1_operator.java
@@ -0,0 +1,30 @@
+public class Test1_operator {
+    public static void main(String[] args){
+        //第一题
+        int a = 10;
+        int b = 10;
+        int c = 10;
+        a = b++;
+        c = --a;
+        b = ++a;
+        a = c--;
+        System.out.println("a = " + a + ",b = " + b + ",c = " + c);
+
+        //第二题
+        int x = 4;
+        int y = (x--) + (++x) + (x*10);
+        System.out.println("x = " + x + ",y = " + y);
+
+        //第三题
+        byte e = 10;
+        e++;
+        System.out.println("e = " + e);
+
+
+        byte f = 10;
+        //f = f + 1;
+        f = (byte)(f+1);
+        System.out.println(f);
+
+    }
+}
