749fa05bc5d256b03a52a417c49dc0ef075225db
lastsonic <yang.hu@cloudwiz.cn>
2019-03-31 08:54:43 +0800

    8th

---
+++ day01/src/Demo1_Operator.java
@@ -0,0 +1,14 @@
+public class Demo1_Operator {
+    public static void main(String[] args){
+        System.out.println(10/3);
+        System.out.println(10/3.0);
+
+        System.out.println(3%5);
+        System.out.println(4%5);
+        System.out.println(-2%5);
+        System.out.println(-12%5);
+        System.out.println(5%5);
+        System.out.println(-5%5);
+        System.out.println(-13%-5);
+    }
+}
