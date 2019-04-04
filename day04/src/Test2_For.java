c654f27640a9fbac8ae8b1e54d471abaa2826559
lastsonic <yang.hu@cloudwiz.cn>
2019-04-04 23:23:12 +0800

    5

--- day04/src/Test2_For.java
+++ day04/src/Test2_For.java
@@ -1,9 +1,21 @@
 class Test2_For {
     public static void main(String args[]){
+        //1-10的和
         int sum = 0;
         for (int i=1;i<=10;i++){
             sum=sum+i;
         }
         System.out.println("sum = " + sum);
+
+        System.out.println("----------------------");
+
+        //1-100的偶数和
+        int sum1=0;
+        for (int i=1;i<=100;i++){
+            if (i%2==0){
+                sum1=sum1+i;
+            }
+        }
+        System.out.println("Sum = " + sum1);
     }
 }
