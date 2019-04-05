b1abe327a75d40de4f33807437b776fee8766677
lastsonic <yang.hu@cloudwiz.cn>
2019-04-05 09:37:57 +0800

    5432

---
+++ day04/src/Test1.java
@@ -0,0 +1,13 @@
+class Test1 {
+    public static void main(String args[]){
+        for (int x=1;x<=10;x++){
+            if (x%3==0){
+                //在此处填写考察代码
+                //break;                        //此处填写break时，会输出两次"java基础"
+                //continue;                     //此处填写continue时，会输出七次"java基础"
+                System.out.println("Java基础");  //填写这个println语句会输出13次
+            }
+            System.out.println("Java基础");
+        }
+    }
+}
