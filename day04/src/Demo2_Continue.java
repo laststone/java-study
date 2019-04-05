168ffd04571307cc0a685e4c96cb0b51d43865f4
lastsonic <yang.hu@cloudwiz.cn>
2019-04-05 09:16:10 +0800

    654

---
+++ day04/src/Demo2_Continue.java
@@ -0,0 +1,11 @@
+class Demo2_Continue {
+    public static void main(String args[]){
+        //continue使用场景：只能在循环中使用
+        for (int x=1;x<=10;x++){
+            if (x==4){
+                continue;
+            }
+            System.out.println("x = " + x);
+        }
+    }
+}
