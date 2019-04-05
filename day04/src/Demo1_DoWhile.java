53d3b860cafb494865964cfec3165297a8ae6803
lastsonic <yang.hu@cloudwiz.cn>
2019-04-05 08:50:42 +0800

    1234

--- day04/src/Demo1_DoWhile.java
+++ day04/src/Demo1_DoWhile.java
@@ -6,5 +6,22 @@ class Demo1_DoWhile {
             i++;
         }
         while (i<=10);
+
+        System.out.println("-------------------");
+        //do...while语句至少要执行一次循环体
+        int n=11;
+        do {
+            System.out.println("n = " + n);
+            n++;
+        }
+        while (n<=10);
+
+        System.out.println("-------------------");
+        //while语句可以不执行循环体
+        int j=11;
+        while (j<=10){
+            System.out.println("j = " + j);
+            j++;
+        }
     }
 }
