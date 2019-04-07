bb5e88ee3763215286011317a56012abc84476da
lastsonic <yang.hu@cloudwiz.cn>
2019-04-07 21:15:09 +0800

    ghjk

---
+++ day05/src/Demo14_Array.java
@@ -0,0 +1,8 @@
+public class Demo14_Array {
+    public static void main(String[] args){
+        int [] [] arr=new  int[3][2];
+        System.out.println(arr);
+        System.out.println(arr[0]);
+        System.out.println(arr[0][0]);
+    }
+}
