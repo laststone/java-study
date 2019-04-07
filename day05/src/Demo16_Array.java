0e6bc3453936191400b3c5f1e315714a5e7a2906
lastsonic <yang.hu@cloudwiz.cn>
2019-04-07 21:24:09 +0800

    ghjk8

---
+++ day05/src/Demo16_Array.java
@@ -0,0 +1,9 @@
+public class Demo16_Array {
+    public static void main(String[] args){
+        int [] [] arr={{1,2,3},{4,5},{6,7,8,9}};   // 定义一个二维数组并赋值
+
+        System.out.println(arr);
+        System.out.println(arr[0]);
+        System.out.println(arr[0][0]);
+    }
+}
